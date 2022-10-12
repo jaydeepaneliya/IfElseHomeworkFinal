b1d43dee41b2f054acf9982e64234e9fbad47acb
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-10-13 05:08:37 +0530

    Upper to lower case Commit

---
+++ src/UpperCaseLowerCase.java
@@ -0,0 +1,10 @@
+public class UpperCaseLowerCase {
+    static String alphabat = "A";
+
+    public static void main(String[] args) {
+        String result;
+        result = alphabat.toLowerCase();
+        System.out.println(result);
+
+    }
+}
