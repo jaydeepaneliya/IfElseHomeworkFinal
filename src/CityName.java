aa0537b8ab59247f24f71305250f3f0186f45f8a
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-10-13 04:53:01 +0530

    City Name Commit

---
+++ src/CityName.java
@@ -0,0 +1,29 @@
+public class CityName {
+    public static void main(String[] args) {
+        char alphabat = 'r';
+        switch(alphabat){
+            case 'a':
+                System.out.println("Ahmedabad");
+                break;
+            case 'b':
+                System.out.println("Baroda");
+                break;
+            case 'c':
+                System.out.println("Chennai");
+                break;
+            case 'd':
+                System.out.println("Delhi");
+                break;
+            case 'e':
+                System.out.println("Edmond");
+                break;
+            case 'f':
+                System.out.println("Fatehpur");
+                break;
+            default:
+                System.out.println("Invalid entry!!!");
+
+        }
+
+    }
+}
