ef9e40b4a7a7437ee81216f408a256ea79dc94b6
Jaydeep Paneliya <jspaneliya1002@gmail.com>
2022-10-13 04:06:17 +0530

    Average of numbers Commit

---
+++ src/Average.java
@@ -0,0 +1,12 @@
+public class Average {
+
+    public static void averageOfNumbers(double a, double b, double c, double d, double e){
+        double avg = (a+b+c+d+e)/5;
+        System.out.println(avg);
+    }
+
+    public static void main(String args[]) {
+        averageOfNumbers(5, 6, 2,4,9);
+    }
+
+}
